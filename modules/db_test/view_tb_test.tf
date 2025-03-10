c4bc1c20a64154d5c2f1e12a72516072d9a24b82
Robson Allef <contato.robsonallef@gmail.com>
2025-03-10 13:04:30 -0300

    remove view creation

--- modules/db_test/view_tb_test.tf
+++ modules/db_test/view_tb_test.tf
@@ -1,7 +1,7 @@
-resource "snowflake_view" "my_view" {
-  database    = "db_test"
-  schema      = "main"
-  name        = "VW_CLIENTE_ATIVO"
-  statement   = "SELECT ID, NOME FROM MEU_CLIENTE WHERE STATUS = 'ATIVO'"
-  is_secure   = false
-}
+# resource "snowflake_view" "my_view" {
+#   database    = "db_test"
+#   schema      = "main"
+#   name        = "VW_CLIENTE_ATIVO"
+#   statement   = "SELECT ID, NOME FROM MEU_CLIENTE WHERE STATUS = 'ATIVO'"
+#   is_secure   = false
+# }
